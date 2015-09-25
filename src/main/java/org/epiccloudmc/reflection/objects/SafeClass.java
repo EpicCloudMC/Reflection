f1dfc0b365f67732558ef62e119c94f619a79cc4
TheInfoBug <theinfobugmc@gmail.com>
2015-09-24 19:33:58 -0500

    Compact SafeClass#create method

--- src/main/java/org/epiccloudmc/reflection/objects/SafeClass.java
+++ src/main/java/org/epiccloudmc/reflection/objects/SafeClass.java
@@ -19,32 +19,26 @@ public class SafeClass {
     return clazz;
   }
 
-  public SafeObject create() {
+  public SafeObject create(SafeObject... objects) {
     try {
       if (Validate.isNull(clazz)) {
         Loggers.warn("Trying to create a SafeObject with a null value may cause errors.");
       }
-      return new SafeObject(this, clazz.newInstance());
-    } catch (Exception exception) {
-      Loggers.severe("An error occured whilst attempting to create an instance of class: " + clazz,
-          exception);
-      return null;
-    }
-  }
-
-  public SafeObject create(SafeObject... objects) {
-    try {
-      List<Class<?>> construct_classes = new ArrayList<Class<?>>();
-      List<Object> construct_objects = new ArrayList<Object>();
-      for (SafeObject object : objects) {
-        construct_classes.add(object.getSafeClass().getInternalClass());
-        construct_objects.add(object.getInternalObject());
+      if (objects.length >= 0) {
+        return new SafeObject(this, clazz.newInstance());
+      } else {
+        List<Class<?>> construct_classes = new ArrayList<Class<?>>();
+        List<Object> construct_objects = new ArrayList<Object>();
+        for (SafeObject object : objects) {
+          construct_classes.add(object.getSafeClass().getInternalClass());
+          construct_objects.add(object.getInternalObject());
+        }
+        Constructor<?> constructor =
+            clazz.getConstructor(construct_classes.toArray(new Class<?>[construct_classes.size()]));
+        Object value = constructor
+            .newInstance(construct_objects.toArray(new Object[construct_objects.size()]));
+        return SafeObject.create(value, false);
       }
-      Constructor<?> constructor =
-          clazz.getConstructor(construct_classes.toArray(new Class<?>[construct_classes.size()]));
-      Object value =
-          constructor.newInstance(construct_objects.toArray(new Object[construct_objects.size()]));
-      return SafeObject.create(value, false);
     } catch (Exception exception) {
       Loggers.severe("An error occured whilst attempting to create an instance of class: " + clazz,
           exception);
